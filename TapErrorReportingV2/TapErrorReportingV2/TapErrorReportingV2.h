c928f06bae9822a70fcc7bd22706a1a6a65715ee
O Rabie <o.rabie@tap.company>
2020-09-22 13:07:52 +0200

    Upgrade to Xcode12+ compiler friendly code

---
+++ TapErrorReportingV2/TapErrorReportingV2/TapErrorReportingV2.h
@@ -0,0 +1,18 @@
+//
+//  TapErrorReportingV2.h
+//  TapErrorReportingV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapErrorReportingV2.
+FOUNDATION_EXPORT double TapErrorReportingV2VersionNumber;
+
+//! Project version string for TapErrorReportingV2.
+FOUNDATION_EXPORT const unsigned char TapErrorReportingV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapErrorReportingV2/PublicHeader.h>
+
+
